b541acb9cc5bdfebf8fb397f1f3c7ed759926c45
bpdavies92 <126164819+bpdavies92@users.noreply.github.com>
2025-09-13 22:13:55 +0000

    Added course principles blocks as a template

---
+++ app/components/CoursePrincipleBlocks.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-sheet color="warning" class="rounded-xl overflow-hidden">
+    <v-img
+    :src="image"
+    cover
+
+    ></v-img>
+    <div class="pa-3">
+      <h3><slot name="principle"></slot></h3>
+    </div>
+  </v-sheet>
+</template>
+
+<script setup>
+  
+  const props = defineProps(['image'])
+
+
+</script>
